510fc980131c421aeb1c3cbc870402b5a7348c6c
Michael <mderazon@gmail.com>
2017-12-27 21:37:04 +0200

    prettier formatting
    and some comsetic changes

--- test/test-charge.js
+++ test/test-charge.js
@@ -1,27 +1,27 @@
-var assert = require('chai').assert;
-var creditguard = require('../lib/creditguard');
+const assert = require('chai').assert;
+const creditguard = require('../lib/creditguard');
 
-var payment_env = {
+const payment_env = {
   language: 'heb',
-  user: "israeli",
-  password: "PASSWORD",
-  server: "https://cguat2.creditguard.co.il",
-  terminal: "TERM"
+  user: 'israeli',
+  password: 'PASSWORD',
+  server: 'https://cguat2.creditguard.co.il',
+  terminal: 'TERM'
 };
 
-var payment_env_with_mid = {
+const payment_env_with_mid = {
   language: 'heb',
-  user: "israeli",
-  password: "PASSWORD",
-  server: "https://cguat2.creditguard.co.il",
-  terminal: "TERM",
-  mid: "MID",
-  success_url: "http://localhost:3000/success",
-  error_url: "http://localhost:3000/error",
-  cancel_url: "http://localhost:3000/cancel"
+  user: 'israeli',
+  password: 'PASSWORD',
+  server: 'https://cguat2.creditguard.co.il',
+  terminal: 'TERM',
+  mid: 'MID',
+  success_url: 'http://localhost:3000/success',
+  error_url: 'http://localhost:3000/error',
+  cancel_url: 'http://localhost:3000/cancel'
 };
 
-var charge = {
+const charge = {
   cardNo: '4012888888881881',
   cardExpiration: '0916',
   id: '310608195',
@@ -48,26 +48,22 @@ var charge = {
   }
 };
 
-
 suite('charge', function() {
   test('successful charge', function(done) {
-    var cg = creditguard(payment_env);
+    const cg = creditguard(payment_env);
     cg.call(charge, function(err, res) {
       assert.isNull(err);
       assert.isNotNull(res);
-      console.log(JSON.stringify(res, false, '\t'));
       done();
     });
   });
 
   test('successful charge with mid and urls', function(done) {
-    var cg = creditguard(payment_env_with_mid);
+    const cg = creditguard(payment_env_with_mid);
     cg.call(charge, function(err, res) {
       assert.isNull(err);
       assert.isNotNull(res);
-      console.log(JSON.stringify(res, false, '\t'));
       done();
     });
   });
-
 });
